30c8cb47669432e15e534b862912cd8b0159fa3b
Connor <connor.chetham@gmail.com>
2024-01-29 21:03:54 +0000

    added view for DB tab

---
+++ Views/DatabaseTab.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using DatabaseApp.Views;
+
+namespace DatabaseApp.Views.DatabaseTab;
+
+public partial class DatabaseTab : UserControl
+{
+    public DatabaseTab()
+    {
+        InitializeComponent();
+    }
+}
